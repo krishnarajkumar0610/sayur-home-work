45cd4cd574e346dc19d3e8ea08eee3ab0d24e8f5
krishnarajkumar0610 <krishanraj0610@gmail.com>
2023-09-04 19:21:24 +0530

    printing the age

--- week1/AgeCalculation.py
+++ week1/AgeCalculation.py
@@ -6,17 +6,11 @@ Ask what year they were born.  get the year from the user. Print 'You are <age> 
 
 def agecalculation():
     name = input("Enter your name :") # asking user name
-    
     print("Hello ",name)
-    
     current_year=int(input("Enter current year :"))  # asking current year from user
-    
     date_of_birth=int(input("Enter the year you are born  :"))   # asking user date of birth
-    
     age=current_year-date_of_birth      # eg:   2023 - 2003 = 20
-    
     # age will be stored in age variable 
-    
     return "You are "+str(age)+" years old" # returned as string
 
 print(agecalculation()) # printing the returned statement
