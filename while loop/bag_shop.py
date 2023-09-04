523786e14fc28d78481b2d9f0f2470e97c69e497
krishnarajkumar0610 <krishanraj0610@gmail.com>
2023-09-04 21:37:10 +0530

    bag shop

--- while loop/bag_shop.py
+++ while loop/bag_shop.py
@@ -10,22 +10,27 @@ totalSales , totalBagsSold = 0,0
 bags=0
 count_red,count_green=0,0
 
-while ( totalBagsSold <10 and totalSales < 10000) :
-    #Ask customer for input
-    choice=input("Enter your choice to buy both bags or green or red :")
-    choice=choice.lower()
-    if choice =="red" or choice =="both":
-        bags=int(input("Enter the number of red bags you need :"))
-        totalBagsSold=totalBagsSold+bags
-        count_red+=bags
-        redBags-=bags
-        totalSales=totalSales+(bags*redBags)
-    if choice =="white" or choice =="both":
+while ( totalBagsSold <10 and totalSales < 10000) :     
+    # while will break when total sales amount is > 10000 or totalbagsSold >10
+    choice=input("Enter your choice to buy both bags or green or red :")    # asking user choice   
+    choice=choice.lower()   # converting it into lower form
+    if choice!="red" and "green" and "both":
+        print("Invalid bag color")
+        continue   
+    if choice =="red" or choice =="both":   # comparing red or both 
+        bags=int(input("Enter the number of red bags you need :"))  # asking number of red bags need 
+        totalBagsSold=totalBagsSold+bags    # adding the bags count in total bags sold
+        count_red+=bags                     # adding the red bags count
+        redBags-=bags                       # after buying the red bags decreasing the red bags count
+        totalSales=totalSales+(bags*redBags)    # making bill for red bags
+        
+    if choice =="white" or choice =="both": # comparing green or both
         bags=int(input("Enter the number of green bags you need :"))
-        totalBagsSold=totalBagsSold+bags
-        count_green+=bags
-        greenBags-=bags
-        totalSales=totalSales+(bags*greenBags)
+        totalBagsSold=totalBagsSold+bags    # adding the bags count in total bags sold
+        count_green+=bags                   # adding the green bags count
+        greenBags-=bags                     # after buying the green bags decreasing the red bags count
+        totalSales=totalSales+(bags*greenBags)  # making bill for green bags    
+        
 if count_red!=0:
     print(f"{count_red} number of red bags were sold ")
 if count_green!=0:
