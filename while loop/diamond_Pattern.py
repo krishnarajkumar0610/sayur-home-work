829e68b69fdf9e6fe4bf9516bb512fb8bfbf7cd0
krishnarajkumar0610 <krishanraj0610@gmail.com>
2023-09-04 21:02:28 +0530

    diamound program

--- while loop/diamond_Pattern.py
+++ while loop/diamond_Pattern.py
@@ -1,9 +1,27 @@
 lines = int(input("Enter number of lines :"))
 number=1
-while(number<=lines):
-    if input("Enter space to print $ :") == " ":
-        print(' '*(lines-number),end='')
-        print(f"{'$ '*number}")
-        number+=1
+while(number<=lines):   # this loops run untill number reaches 10. After reaching 10 and executing the loop it will break the loop
+    if input("Enter space to print $ ") == " ": # asking user to click space bar to execute if statement
+        print(' '*(lines-number),end='')        # printing  (" " * number of lines - loop variable)
+        print(f"{'$ '*number}")     # printing "$ " * loop variable
+        number+=1   # increamenting loop variable
     else:
         break
+
+
+
+
+# lines = int(input("Enter number of lines :"))
+# number=1
+# temp_num=number
+# while(lines>temp_num):
+#     if number<=5:
+#         if input("Enter space to print $ :") == " ":
+#             print(' '*(lines - number),end='')
+#             print(f"{'$ '*temp_num}")
+#             temp_num+=1
+#             lines-=1
+#         else:
+#             break
+#     else:
+#         number=1
