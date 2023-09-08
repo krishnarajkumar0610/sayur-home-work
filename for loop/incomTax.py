c1463464e9a51c70c728967a31f3302b48f457e5
krishnarajkumar0610 <krishanraj0610@gmail.com>
2023-09-08 22:10:23 +0530

    incometax

--- for loop/incomTax.py
+++ for loop/incomTax.py
@@ -6,6 +6,37 @@ Rs. 9,00,000 to Rs. 12,00,000	        Rs. 45,000 + 15% on income more than Rs. 9
 Rs. 12,00,000 to Rs. 1500,000	        Rs. 90,000 + 20% on income more than Rs. 12,00,000
 Above Rs. 15,00,000	                    Rs. 150,000 + 30% on income more than Rs. 15,00,000
 """
+
+salary = int(input("enter your salary : "))
+income_slabs = [300000 , 600000 , 900000 , 1200000 , 1500000]
+tax_per = [0.05 , 0.10 , 0.15 , 0.20 , 0.3]
+tax = 0
+tax_sort = 0
+for i in range(len(income_slabs)):
+        if salary < income_slabs[i]:
+            tax = 0
+            break
+        if i == len(income_slabs) - 1 :
+            tax = tax_sort + ((income_slabs[i]-salary)tax_per[i])
+            break
+        elif income_slabs[i] < salary <= income_slabs[i+1] :
+            tax = tax_sort + ((salary - income_slabs[i])tax_per[i])
+            break
+        else:
+            tax_sort += income_slabs[0]*tax_per[i] 
+print(tax)
+
+#800000 - 0 + 15000 + 20000 
+
+# salary = int(input())   
+# slabs=[300000,600000,900000,1200000,1500000]
+# percentage=[0.05,0.1,0.15,0.2,0.3]
+# previous_tax=[0,15000,45000,90000,150000]    
+# tax=0
+# for i in range(0,len(slabs)):
+#     if salary<=slabs[i]:
+#         tax = previous_tax[i] + (percentage[i] * (salary - slabs[i]))
+# print(f"Income tax : {tax}")
 # incom_taxes=[300000,600000,900000,1200000,1500000]
 # tax_percentage=[0.05,0.10,0.15,0.20,0.30]
 # if len(incom_taxes)!=len(tax_percentage):
@@ -37,34 +68,21 @@ Above Rs. 15,00,000	                    Rs. 150,000 + 30% on income more than Rs
 #             cumulative_tax = cumulative_tax + (slabs[i]*tax_per[i])
 # print(f"Your tax amount is :{tax}") 
                     
+# salary = int(input("Enter your salary : "))
+# if salary <= 300000:
+#     tax = "nil"
 
-salary = int(input("Enter your salary : "))
-if salary <= 300000:
-    tax = "nil"
+# elif salary> 300000 and salary<=600000:   
+#     tax = 0 + (0.05 * (salary - 300000))
 
-elif salary> 300000 and salary<=600000:
-    tax = 0 + (0.05 * (salary - 300000))
+# elif salary>600000 and salary<=900000:
+#     tax = 15000 + (0.1 * (salary - 600000))
 
-elif salary>600000 and salary<=900000:
-    tax = 15000 + (0.1 * (salary - 600000))
+# elif salary>900000 and salary<=1200000:
+#     tax = 45000 + (0.15 * (salary - 900000))
 
-elif salary>900000 and salary<=1200000:
-    tax = 45000 + (0.15 * (salary - 900000))
+# elif salary>1200000 and salary<=1500000:
+#     tax = 90000 + (0.2 * (salary - 1200000))
 
-elif salary>1200000 and salary<=1500000:
-    tax = 90000 + (0.2 * (salary - 1200000))
-
-else:
-    tax = 150000 + (0.3 * (salary - 1500000))
-salary = int(input())   
-slabs=[300000,600000,900000,1200000,1500000]
-percentage=[0.05,0.1,0.15,0.2,0.3]
-previous_tax=[0,15000,45000,90000,150000]    
-tax=0
-for i in range(0,len(slabs)):
-    if salary<=slabs[i]:
-        tax = previous_tax[i] + (percentage[i] * (salary - slabs[i]))
-
-
-
-print(f"Income tax : {tax}")
+# else:
+#     tax = 150000 + (0.3 * (salary - 1500000))
--- while loop/add_a_end.py
+++ while loop/add_a_end.py
@@ -4,18 +4,25 @@
     Output - cataa, arrow (length is equal by adding a)'''
 name_one=input("Enter name 1 :")    # asking user name one
 
-name_two=input("Enter name 2 :")    # # asking user name two
+name_two=input("Enter name 2 :")    # # asking user name one
 
-if(len(name_one)==len(name_two)):
+length_name_one=len(name_one)   # storing name one length
+
+length_name_two=len(name_two)   # storing name two length
+
+if(length_name_one==length_name_two):   # comparing 2 names length if it equals it true
     print("Already both length are equal")
-else:
-    while(length_name_one!=length_name_two):
-        if length_name_one<length_name_two:
-            name_one=name_one+'a'
-            length_name_one=len(name_one)
-        elif length_name_one>length_name_two:
-            name_two=name_two+'a'
-            length_name_two=len(name_two)
+    
+else:   # if it is not equal it comes else statement
+    while(length_name_one!=length_name_two):    # it stops when my both name one and name two length becomes equal
+        if length_name_one<length_name_two: # if name one length < name two, add 'a' in name one at end
+            name_one=name_one+'a'   # adding a at end in name one
+            length_name_one=len(name_one)   # storing name one length
+            
+        elif length_name_one>length_name_two:   # if name two length < name one, add 'a' in name two at end
+            name_two=name_two+'a'      # adding a at end in name one
+            length_name_two=len(name_two)   # storing name one length
+            
 print(f"name one :{name_one}")
 print(f"Name two :{name_two}")
 
