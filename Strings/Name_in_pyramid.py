b93052d52299b77a6411ede77580a40d08b66667
krishnarajkumar0610 <krishanraj0610@gmail.com>
2023-09-10 12:31:15 +0530

    print name in a pyramid form

---
+++ Strings/Name_in_pyramid.py
@@ -0,0 +1,27 @@
+########## Program 2
+#Print your name in a pyramid
+#eg RAM
+#R
+#RA
+#RAM
+
+myName = input("Enter your name :") # asking user name
+for i in range(len(myName)):    # stoing index values of myname in 'i'
+    
+    for j in range(0,i+1):      # j is always st with 0 and ends with i value
+        
+        print(myName[j],end='') # printing the characters up to ith index value
+        
+    print() # goes to next line after printing one line
+
+
+#OUTPUT
+
+# Enter your name :krishna
+# k      
+# kr     
+# kri    
+# kris   
+# krish  
+# krishn 
+# krishna
