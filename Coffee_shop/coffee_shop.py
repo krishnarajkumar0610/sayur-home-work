26a19e21b7999645b7a72f4329caed05af86dbd8
krishnarajkumar0610 <krishanraj0610@gmail.com>
2023-09-15 22:40:14 +0530

    run a cafe

--- Coffee_shop/coffee_shop.py
+++ Coffee_shop/coffee_shop.py
@@ -23,3 +23,55 @@ if total<total_costPrize:
     print(f"You had lost because your total profit = {total} is < total cost price = {total_costPrize}")
 elif total>total_costPrize:
     print(f"You had get profit your total profit = {total} is > total cost price = {total_costPrize}")
+    
+    
+''' 
+OUTPUT - 1 
+------------
+
+Enter your food item :coffee
+Enter number of items coffee you need ? 3
+Enter 1 to cancel order or 0 to continue ordeer :0
+Enter your food item :tea
+Enter number of items tea you need ? 3
+Enter 1 to cancel order or 0 to continue ordeer :0
+Enter your food item :greentea
+Enter number of items greentea you need ? 3
+Enter 1 to cancel order or 0 to continue ordeer :0
+Enter your food item :blacktea
+Enter number of items blacktea you need ? 5
+Enter 1 to cancel order or 0 to continue ordeer :0
+Enter your food item :tea
+Enter number of items tea you need ? 5
+Enter 1 to cancel order or 0 to continue ordeer :1
+Your Ordered list : {'coffee': 3, 'tea': 5, 'greentea': 3, 'blacktea': 5}
+Your total bill amount is : 495
+You had lost because your total profit = 495 is < total cost price = 500
+
+------------
+
+OUTPUT - 2
+----------
+
+Enter your food item :coffee
+Enter number of items coffee you need ? 5
+Enter 1 to cancel order or 0 to continue ordeer :0
+Enter your food item :tea
+Enter number of items tea you need ? 5
+Enter 1 to cancel order or 0 to continue ordeer :0
+Enter your food item :greentea
+Enter number of items greentea you need ? 8
+Enter 1 to cancel order or 0 to continue ordeer :0
+Enter your food item :blacktea
+Enter number of items blacktea you need ? 6
+Enter 1 to cancel order or 0 to continue ordeer :0
+Enter your food item :tea
+Enter number of items tea you need ? 4
+Enter 1 to cancel order or 0 to continue ordeer :1
+Your Ordered list : {'coffee': 5, 'tea': 4, 'greentea': 8, 'blacktea': 6}
+Your total bill amount is : 745
+You had get profit your total profit = 745 is > total cost price = 500 
+
+----------
+
+'''
