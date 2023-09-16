4d9bf13d019bc3f0352fc8db50f4dcb2ce7c9d26
krishnarajkumar0610 <krishanraj0610@gmail.com>
2023-09-16 22:53:40 +0530

    user name & password

--- manipulationExercise/nameAndPass_crt.py
+++ manipulationExercise/nameAndPass_crt.py
@@ -4,10 +4,10 @@
 #      name of the company mentioned in the username, followed by any 3 numbers.
 #      eg username : myname@sayur.com. password - mnamesay123
 
-name = input("Enter your name & contain the char @ and .com or .edu or .tech or org at the end :")
+name = input("Enter your name & contain the char @ and .com or .edu or .tech or .org at the end :")
 password=input("Enter your password : ")
 special_char = "@"  # initializing special character "@"
-name_conditions=[".com",".edu",".tech","org"]  # initialing all conditions we need 
+name_conditions=[".com",".edu",".tech",".org"]  # initialing all conditions we need 
 first_let=name[0]
 third_let=name[2]
 lastThree_let=""
