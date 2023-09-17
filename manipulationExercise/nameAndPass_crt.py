9c12376e6304039068067034dc9507e427f461b3
krishnarajkumar0610 <krishanraj0610@gmail.com>
2023-09-17 11:15:23 +0530

    user name & password

--- manipulationExercise/nameAndPass_crt.py
+++ manipulationExercise/nameAndPass_crt.py
@@ -1,7 +1,8 @@
-# ########### Program 5 #######
-  # Write a program to run a cafe
-  #  Cafe should have inventory, sales data and profit
-
+# Check if the username and password is correct. 
+#      Username should contain the char @  and '.com' or '.edu' or '.tech' or 'org' at the end.
+#      passward is the first, third, and last 3 letters of the username followed by the first three letters of the 
+#      name of the company mentioned in the username, followed by any 3 numbers.
+#      eg username : myname@sayur.com. password - mnamesay123 
 # method 1
 
 name = input("Enter your name & contain the char @ and .com or .edu or .tech or .org at the end :")
