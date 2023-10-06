b60199a56f85c945e82dbd8139085e32fc44e687
krishnarajkumar0610 <krishanraj0610@gmail.com>
2023-10-06 20:20:37 +0530

    Logical thinking

--- Logical thinking/PrintLetAtoA.py
+++ Logical thinking/PrintLetAtoA.py
@@ -3,15 +3,19 @@ def printLetters(aString):
     last_a=0
     if first_a>=0:
         last_a=aString.rfind('a')
-        if last_a != last_a:
+        if first_a != last_a:
             print(aString[first_a+1:last_a])
         else:
-            print("There is only onr 'a' ")
+            print("There is only one 'a' ")
     else:
         print("There is no 'a' ")
-        
-
 
 sentence = input("Enter the sentence : ")
 sentence=sentence.lower()
 printLetters(sentence)
+
+
+# OUTPUT
+
+# Enter the sentence : i am krishna rajkumar
+# m krishna rajkum
