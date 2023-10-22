ce9c0324100d9f08d6725317ac939bb6781584fd
krishnarajkumar0610 <krishnaraj0610@gmail.com>
2023-10-22 10:02:06 +0530

    recursion

---
+++ recursion/recursion.py
@@ -0,0 +1,13 @@
+def fibo(n):  
+   if n <= 1:  
+       return n  
+   else:  
+       return(fibo(n-1) + fibo(n-2))  
+
+n=int(input("Enter the nth number to get fibonacci series : "))
+if n<0:
+    print("Enter number which is >=0")
+else:
+    print(f"Fibonacci numbers for {n} :",end=' ')  
+    for i in range(n+1):  
+        print(fibo(i),end=' ')
