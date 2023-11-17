fb9a17a942f5cada23c13459a8316e50bac045fb
krishnarajkumar0610 <krishnaraj0610@gmail.com>
2023-11-18 03:42:56 +0530

    shop using class and object

--- class_obj/shop.py
+++ class_obj/shop.py
@@ -9,25 +9,30 @@ class Shop: # creating the class
         self.stocks=[50,50,30]
         
     def menuItems(self):    # menu function to show the menu 
+        print("-----------------------------")
         print(f"Snakcs : {self.items}")
         print(f"Prices : {self.prices}")
         print(f"Stocks : {self.stocks}")
-
+        print("-----------------------------\n")
     def buyItem(self,item,quantity,):   # buying snacks function
         self.item=item
         for index,product in enumerate(self.items):
             if product in item: # if my product is found then i'm subtracting the quantity to stock
                 self.total_price=quantity*self.prices[index]    # number of quantity * price of the item is stored in price
                 self.stocks[index]-=quantity
+                print("-----------------------------")
                 print(f"Ordered item is  : {self.item}")
                 print(f"Quantity : {quantity}")
                 print(f"Total Price : {self.total_price}")
+                print("-----------------------------\n")
                 break
             
     def show(self):
+        print("-----------------------------")
         print(f"Available items : {self.items}")
         print(f"Prices : {self.prices}")
         print(f"Stocks : {self.stocks}")
+        print("-----------------------------\n")
                 
 s = Shop()
 s.menuItems()
