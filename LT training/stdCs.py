6c0d9e3cdc1a5648aef4d81d00032ebcc62dea3f
krishnarajkumar0610 <krishnaraj0610@gmail.com>
2023-11-18 16:05:18 +0530

    pass and failed stuednts in cs subject

--- LT training/stdCs.py
+++ LT training/stdCs.py
@@ -5,24 +5,25 @@
 # Print a new list with all the students with pass marks along with their mark in the format name:mark.
 # Also print the number of students who failed.
 
-students = ['krishna','raj','kumar','ramu','somu']
+students = ['krishna','raj','kumar','ramu','somu']  # students name list
 
-cs_marks = [98,87,50,49,25]
+cs_marks = [98,87,50,49,25] # students mark list
 
-pass_count,fail_count=0,0
+pass_count,fail_count=0,0   #assigning 0 to pass and fail count
 
-pass_std,fail_std=[],[]
+pass_std,fail_std=[],[] # assigning empty list to pass and fail student list
 
 
-pass_mark=50
+pass_mark=50    # assigning pass mark 50 to pass mark variable
 
-for index,student in enumerate(students):
-    if(cs_marks[index]>=pass_mark):
-        pass_std.append(f"{student} : {str(cs_marks[index])}")
-        pass_count+=1
-    else:
-        fail_std.append(f"{student} : {str(cs_marks[index])}")
-        fail_count+=1
+for index,student in enumerate(students):   # using for loop to traverse in the list and enumerate to get index and value
+    if(cs_marks[index]>=pass_mark): # if cs mark of 1 student is >=50 he is pass
+        pass_std.append(f"{student} : {str(cs_marks[index])}")  # adding his name in pass student list
+        pass_count+=1   # counting him
         
-print(f"Pass students list : {pass_std} and their total count is = {pass_count}\n")
-print(f"fail students list : {fail_std} and their total count is = {fail_count}")
+    else:   # else he is fail in cs
+        fail_std.append(f"{student} : {str(cs_marks[index])}")      # adding his name in fail student list
+        fail_count+=1   # couting him
+        
+print(f"Pass students list : {pass_std} and their total count is = {pass_count}\n") # displaying the passed students in cs subject
+print(f"fail students list : {fail_std} and their total count is = {fail_count}")   # displaying the failed students in cs subject
