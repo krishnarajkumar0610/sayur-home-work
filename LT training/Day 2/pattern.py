b3150a7d1a1cf6a6e417b9baa9b8fc6e6d481f31
krishnarajkumar0610 <krishnaraj0610@gmail.com>
2023-11-24 15:42:14 +0530

    Day 2

--- LT training/Day 2/pattern.py
+++ LT training/Day 2/pattern.py
@@ -10,7 +10,7 @@ previous=''  # creating this variable to store the previous row numbers
 n = int(input("Enter number of rows you need : "))  # asking the input for number of rows
 temp=n-1
 for index in range(1,n+1):  # this for loop is to run 5 times
-    print(' '*temp,end='')
+    # print(' '*temp,end='')
     temp-=1
     tot_str=str(index)+previous # adding the current index + previous row as string
     print(tot_str)  # printing the total String
