e71e9d76c69aad5a9d961521c1df89a5889e76c1
krishnarajkumar0610 <krishnaraj0610@gmail.com>
2023-11-27 21:48:58 +0530

    Day 3

--- LT training/Day 3/unique.py
+++ LT training/Day 3/unique.py
@@ -4,32 +4,32 @@
 
 
 def check(words,word):
-    if word in words:   # if the word in unique word
-        return True # then returning true
-    return False    # not having returning false
+    if word in words:               # if the word in unique word
+        return True                 # then returning true
+    return False                    # not having returning false
 
-words=[]    # to store the words from the file
-unique_words=[] # to store unique words
-unique_words_count={}   # to store unique words and its count
+words=[]                            # to store the words from the file
+unique_words=[]                     # to store unique words
+uniqueWords_withCount={}               # to store unique words and its count
 
 
-try:    # using exception Handling to check my file is available or not
+try:                                                                    # using exception Handling to check my file is available or not
     with open('F:\Sayur learning\sayur-home-work\LT Training\Day 3\passage_two.txt','r') as file:   # reading my file
         for word_row in file:   
             for word in word_row.strip().lower().split(' '):
                 words.append(word)                           
-    for index,index_word in enumerate(words): # taking the index and its word     
+    for index,index_word in enumerate(words):                           # taking the index and its word     
         if len(index_word) == 4 and not check(unique_words,index_word): # if my word length is 4 and it is not in unique list
-            unique_words.append(index_word) # adding it in unique list
-            count=0 # setting counter as 0
-            for j in range(index,len(words)):   # this loop is to traverse the words list
-                if words[j] == index_word:  # if my word is equals to other word
-                    count+=1    # counting it
-            unique_words_count[index_word]=count    # storing it in the dict with its count from the words
+            unique_words.append(index_word)                             # adding it in unique list
+            count=0                                                     # setting counter as 0
+            for j in range(index,len(words)):                           # this loop is to traverse the words list
+                if words[j] == index_word:                              # if my word is equals to other word
+                    count+=1                                            # counting it
+            uniqueWords_withCount[index_word]=count                        # storing it in the dict with its count from the words
          
-    print(unique_words_count)  # printing the unique words with its count
+    print(uniqueWords_withCount)                                           # printing the unique words with its count
     
-except Exception:   # if my file is not available then exception will be thrown
+except Exception:                                                       # if my file is not available then exception will be thrown
     print("File is not found")
     
     
