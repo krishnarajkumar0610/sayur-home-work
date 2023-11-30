b41e07ddfbe2522fd052a68ef14dba1f20e8b8ec
krishnarajkumar0610 <krishnaraj0610@gmail.com>
2023-11-30 20:05:12 +0530

    Day 5

--- LT training/Day 5/theatre_two.py
+++ LT training/Day 5/theatre_two.py
@@ -1,3 +1,11 @@
+# Problem #2
+# Same problem as above. Also calculate tickets price. 
+# Firsrt 3 rows - Rs100
+# Rows 4 to 12 - Rs 200
+# Rows 13 till top - Rs 300.
+# 3 seats close to the wall on both sides costs less than the other seats in the same row.
+
+
 def display(seats):
     row = 65
     for outer_loop in seats:
